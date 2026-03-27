bdc8167a0a7e2ea5f13861c3ddbfd823437eccf6
pablodev14 <j.p.v1418@gmail.com>
2026-03-27 09:32:34 -0500

    Organizo nuevamente errores de ciclo, orden de opciones, fibonacci y primos

--- Ciclo.java
+++ Ciclo.java
@@ -1,5 +1,6 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Ciclo {
 
@@ -10,7 +11,7 @@ public class Ciclo {
             Scanner sc = new Scanner(System.in);
             int opcion = 0;
 
-            while (opcion != 1) {
+            while (opcion != 5) {
 
                 System.out.println("  ╔╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╗");
                 System.out.println("  ╠╬╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╬╣");
@@ -23,70 +24,111 @@ public class Ciclo {
                 System.out.println("  ╠╣   ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝                    ╠╣");
                 System.out.println("  ╠╣                                                            ╠╣");
                 System.out.println("  ╠╬╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╬╣");
+                System.out.println("  ╠╬╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╬╣");
+                System.out.println("  ╠╣                         MENÚ                               ╠╣");
                 System.out.println("  ╠╣                                                            ╠╣");
-                System.out.println("  ╠╣                         1. Salir                           ╠╣");
-                System.out.println("  ╠╣                                                            ╠╣");
-                System.out.println("  ╠╣              2. Suma N-avo número de fibonacci             ╠╣");
-                System.out.println("  ╠╣                                                            ╠╣");
-                System.out.println("  ╠╣                   3. N-avo numero primo                    ╠╣");
-                System.out.println("  ╠╣                                                            ╠╣");
-                System.out.println("  ╠╣               4. Raíces ecuación cuadratica                ╠╣");
-                System.out.println("  ╠╣                                                            ╠╣");
-                System.out.println("  ╠╣                    5. Promedio n numeros                   ╠╣");
+                System.out.println("  ╠╣   1. Serie de Fibonacci                                    ╠╣");
+                System.out.println("  ╠╣   2. N-ésimo número primo                                  ╠╣");
+                System.out.println("  ╠╣   3. Raíces ecuación cuadrática                            ╠╣");
+                System.out.println("  ╠╣   4. Promedio n números                                    ╠╣");
+                System.out.println("  ╠╣   5. Salir                                                 ╠╣");
                 System.out.println("  ╠╣                                                            ╠╣");
                 System.out.println("  ╠╬╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╦╬╣");
                 System.out.println("  ╚╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╩╝");
 
+                System.out.print("Seleccione una opción: ");
                 opcion = sc.nextInt();
 
-                if (opcion == 2) {
+                if (opcion == 1) {
 
                     System.out.println();
-                    System.out.println("  +-----------------------------------------+");
-                    System.out.println("  |   Suma N-avo numero de fibonacci         ");
-                    System.out.println("  +-----------------------------------------+");
+                    System.out.println("+------------------------------+");
+                    System.out.println("|   Serie de Fibonacci         |");
+                    System.out.println("+------------------------------+");
 
-                    int a = 0, b = 1, c = 0, n;
+                    System.out.print("Ingrese el N: ");
+                    int n = sc.nextInt();
 
-                    System.out.println("Ingrese el N-Esimo numero de Fibonacci que quiere mostrar: ");
-                    n = sc.nextInt();
+                    BigInteger a = BigInteger.ZERO;
+                    BigInteger b = BigInteger.ONE;
+                    BigInteger c;
 
-                    System.out.println("  +-----------------------------------------+");
-                    do {
-                        System.out.println("  |  " + a);
-                        c = a + b;
+                    System.out.println("+------------------------------+");
+
+                    for (int i = 0; i < n; i++) {
+                        System.out.println("| " + a);
+                        c = a.add(b);
                         a = b;
                         b = c;
-                    } while (--n > 0);
-                    System.out.println("  +-----------------------------------------+");
+                    }
+
+                    System.out.println("+------------------------------+");
+
+                } else if (opcion == 2) {
+
                     System.out.println();
+                    System.out.println("+------------------------------+");
+                    System.out.println("|   N-ésimo número primo       |");
+                    System.out.println("+------------------------------+");
+
+                    System.out.print("Ingrese la posición (N): ");
+                    int n = sc.nextInt();
+
+                    int contador = 0;
+                    int numero = 2;
+
+                    while (contador < n) {
+
+                        boolean esPrimo = true;
+
+                        for (int i = 2; i <= Math.sqrt(numero); i++) {
+                            if (numero % i == 0) {
+                                esPrimo = false;
+                                break;
+                            }
+                        }
+
+                        if (esPrimo) {
+                            contador++;
+                        }
+
+                        numero++;
+                    }
+
+                    System.out.println("+------------------------------+");
+                    System.out.println("| El primo #" + n + " es: " + (numero - 1));
+                    System.out.println("+------------------------------+");
 
                 } else if (opcion == 3) {
 
                     System.out.println();
                     System.out.println("  +-----------------------------------------+");
-                    System.out.println("  |   N-avo numero primo                    |");
+                    System.out.println("  |   Raíces ecuación cuadrática            |");
+                    System.out.println("  |        ax^2  +  bx  +  c  =  0          |");
                     System.out.println("  +-----------------------------------------+");
 
-                    int divisores = 2, n, contador = 2;
+                    double a, b, c, x1, x2, d;
 
-                    System.out.println("Ingrese el N-Esimo numero primo que quiere mostrar: ");
-                    n = sc.nextInt();
+                    System.out.print("Ingrese valor de a: ");
+                    a = sc.nextDouble();
+                    System.out.print("Ingrese valor de b: ");
+                    b = sc.nextDouble();
+                    System.out.print("Ingrese valor de c: ");
+                    c = sc.nextDouble();
 
-                    while (contador < n) {
-                        if (n % contador == 0) {
-                            divisores++;
-                            break;
-                        }
-                        contador++;
-                    }
+                    d = (b * b) - (4 * a * c);
 
                     System.out.println("  +-----------------------------------------+");
-                    if (divisores > 2) {
-                        System.out.println("  |  El numero " + n + " no es primo");
+
+                    if (d < 0) {
+                        System.out.println("  |  ERROR: no hay soluciones reales");
                     } else {
-                        System.out.println("  |  El numero " + n + " es primo");
+                        x1 = (-b + Math.sqrt(d)) / (2 * a);
+                        x2 = (-b - Math.sqrt(d)) / (2 * a);
+                        System.out.println("  |  X1 = " + x1);
+                        System.out.println("  |  X2 = " + x2);
                     }
+
                     System.out.println("  +-----------------------------------------+");
                     System.out.println();
 
@@ -94,66 +136,44 @@ public class Ciclo {
 
                     System.out.println();
                     System.out.println("  +-----------------------------------------+");
-                    System.out.println("  |   Raices ecuacion cuadratica            |");
-                    System.out.println("  |        ax^2  +  bx  +  c  =  0          |");
+                    System.out.println("  |   Promedio n números                    |");
                     System.out.println("  +-----------------------------------------+");
 
-                    double a, b, c;
-                    double primerX, segundoX, d;
+                    System.out.print("Ingrese la cantidad de números: ");
+                    int n = sc.nextInt();
 
-                    System.out.println("Ingrese valor de a: ");
-                    a = sc.nextDouble();
-                    System.out.println("Ingrse valor de b: ");
-                    b = sc.nextDouble();
-                    System.out.println("Ingrese valor de c: ");
-                    c = sc.nextDouble();
+                    double suma = 0;
 
-                    d = (b * b) - (4 * a * c);
-
-                    System.out.println("  +-----------------------------------------+");
-                    if (d < 0) {
-                        System.out.println("  |  ERROR: el numero debe ser mayor a 0");
-                    } else {
-                        primerX = -b + Math.sqrt(d) / (2 * a);
-                        segundoX = -b - Math.sqrt(d) / (2 * a);
-                        System.out.println("  |  X1 = " + primerX);
-                        System.out.println("  |  X2 = " + segundoX);
+                    for (int i = 1; i <= n; i++) {
+                        System.out.print("Ingrese número " + i + ": ");
+                        double num = sc.nextDouble();
+                        suma += num;
                     }
+
+                    double promedio = suma / n;
+
+                    System.out.println("  +-----------------------------------------+");
+                    System.out.println("  |  Promedio = " + promedio);
                     System.out.println("  +-----------------------------------------+");
                     System.out.println();
 
                 } else if (opcion == 5) {
 
                     System.out.println();
-                    System.out.println("  +-----------------------------------------+");
-                    System.out.println("  |   Promedio n numeros                    |");
-                    System.out.println("  +-----------------------------------------+");
+                    System.out.println("Saliendo del programa...");
 
-                    System.out.println("Ingrese la cantidad de numeros: ");
-                    int n = sc.nextInt();
-                    double suma = 0;
-                    double promedio;
+                } else {
 
-                    for (int i = 1; i <= n; i++) {
-                        suma = suma + i;
-                    }
-
-                    promedio = suma / 2;
-
-                    System.out.println("  +---------------------------------------------------------------+");
-                    System.out.println("    |  Promedio de los primeros " + n + " numeros es: " + promedio);
-                    System.out.println("  +---------------------------------------------------------------+");
                     System.out.println();
-
-                } else if (opcion != 1) {
-                    System.out.println();
-                    System.out.println("  /!\\ ERROR: Opcion invalida");
+                    System.out.println("  /!\\ ERROR: Opción inválida");
                     System.out.println();
                 }
             }
 
+            sc.close();
+
         } catch (InputMismatchException e) {
-            System.out.println("Error: Debe ingresar un valor valido.");
+            System.out.println("Error: Debe ingresar un valor válido.");
         }
     }
 }
